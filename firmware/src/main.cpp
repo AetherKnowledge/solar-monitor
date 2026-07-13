05777d09bd17d20cdf3410e82043817d9d2d9781
John Christian Rosuelo <johnchristianrosuelo@gmail.com>
2026-07-13 16:30:02 +0800

    init

---
+++ firmware/src/main.cpp
@@ -0,0 +1,49 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <LittleFS.h>
+
+const char* ssid = "GlobeAtHome";
+const char* password = "donotopen8";
+
+AsyncWebServer server(80);
+
+#define LED_PIN 2
+
+void setup() {
+  Serial.begin(115200);
+  // put your setup code here, to run once:
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH);
+
+  WiFi.begin(ssid, password);
+
+    Serial.print("Connecting");
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println();
+    Serial.println("Connected!");
+
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+
+    if (!LittleFS.begin()) {
+        Serial.println("LittleFS Mount Failed");
+        return;
+    }
+
+    server.serveStatic("/", LittleFS, "/")
+          .setDefaultFile("index.html");
+
+    server.begin();
+
+    Serial.println("Web server started");
+  
+}
+
+void loop() {
+}
---
+++ web/vite.config.ts
@@ -0,0 +1,12 @@
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [tailwindcss(), svelte()],
+  build: {
+    outDir: "../firmware/data",
+    emptyOutDir: true,
+  },
+});
