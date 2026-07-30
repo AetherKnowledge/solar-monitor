0f2bfca736d98547ebe2ad658ccd3d508c07d3bf
John Christian Rosuelo <johnchristianrosuelo@gmail.com>
2026-07-30 21:14:51 +0800

    weird bug code mockup not appearing correctly in the logs page without this

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -5,4 +5,13 @@
 
 <ScrollPage>
 	<Dashboard />
+
+	<!-- weird bug code mockup not appearing correctly in the logs page without this -->
+	{#if false}
+		<div class="mockup-code w-full">
+			<pre data-prefix="$"><code>npm i daisyui</code></pre>
+			<pre data-prefix=">" class="text-warning"><code>installing...</code></pre>
+			<pre data-prefix=">" class="text-success"><code>Done!</code></pre>
+		</div>
+	{/if}
 </ScrollPage>
